707dc897e41865d9944d08980bbd444fc6dafe75
Pedro Vinicius <89553913+PedroViniciussd@users.noreply.github.com>
2024-05-08 22:46:23 +0000

    Configuração das 'props' no componente '<Card/>'

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,6 +1,9 @@
-export default function Card() {
-    return <div className='card'> 
-    <h1>Who is this pokemon?</h1>
-    <img src="https://www.salvatore.academy/devmon/2_kotlin.png" width="200" />
+export default function Card(props) {
+
+    return (
+       <div className='card'> 
+    <h1>{props.nome}</h1>
+    <img src={props.imagem} width="200" />
   </div>
+    )
 }
